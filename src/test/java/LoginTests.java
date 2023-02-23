64ec498062f92980661b5b08f09f38008a5b1108
DmitriiGreen <125938716+DmitriiGreen@users.noreply.github.com>
2023-02-22 20:16:15 -0800

    Homework16
    Created a testcase in Selenium for RegistrationnNavigation

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,6 +1,4 @@
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -15,7 +13,7 @@ public class LoginTests extends BaseTest {
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        String url = "https://app.testpro.io/";
+        String url = "https://bbb.testpro.io/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
