db9b3132e25f03ca93f69a459ef23939ca8e40a5
Maria_Hai <126021356+Maria-A33@users.noreply.github.com>
2023-02-24 18:15:05 -0500

    updating Homework16

--- src/test/java/Homework16.java
+++ src/test/java/Homework16.java
@@ -7,17 +7,24 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class LoginTests extends BaseTest {
+public class Homework16 extends BaseTest {
 
     @Test
-    public static void LoginEmptyEmailPasswordTest() {
+    public static void registrationNavigation() {
 
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         String url = "https://bbb.testpro.io/";
+        String url1 = "https://bbb.testpro.io/registration.php";
         driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
+        WebElement registrationLink = driver.findElement(By.cssSelector("[id = 'hel']"));
+                registrationLink.click();
+        Assert.assertNotEquals(driver.getCurrentUrl(), url);
+        Assert.assertEquals(driver.getCurrentUrl(), url1);
         driver.quit();
     }
+
+
+
 }
