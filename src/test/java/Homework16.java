5d7195d58c1c6e363bc3e1de910ab6dd65ca32d8
ElenaNicoara <125710494+ElenaNicoara@users.noreply.github.com>
2023-02-23 19:54:59 -0500

    Homework16

---
+++ src/test/java/Homework16.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Homework16 extends BaseTest {
+
+    @Test
+    public static void registrationNavigation() throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        String url = "https://bbb.testpro.io/";
+        driver.get(url);
+        Thread.sleep(2000);
+        // Access the registration Link
+        WebElement registrationLink = driver.findElement(By.cssSelector("a[id = 'hel']"));
+        registrationLink.click();
+        Thread.sleep(2000);
+        String registrationUrl = "https://bbb.testpro.io/registration.php";
+        Assert.assertEquals(driver.getCurrentUrl(), registrationUrl);
+        driver.quit();
+    }
+}
