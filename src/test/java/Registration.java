6bfc8d38fc3399ae4d4b20aac14088913de492de
janezelenov <118363437+janezelenov@users.noreply.github.com>
2023-02-23 19:38:20 -0500

    Registration test

--- src/test/java/Registration.java
+++ src/test/java/Registration.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -7,24 +8,25 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class LoginTests extends BaseTest {
 
+public class Registration extends BaseTest {
     @Test
-    public static void LoginEmptyEmailPasswordTest() throws InterruptedException {
+    public static void GoingToRegistrationPage () throws InterruptedException {
 
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         String url = "https://bbb.testpro.io/";
         driver.get(url);
-//finding email field
 
-        WebElement loginField = driver.findElement(By.cssSelector("[type='email']"));
+        WebElement registrationField = driver.findElement(By.cssSelector("[id='hel']"));
 
-loginField.sendKeys("janezelenova@gmail.com");
-        driver.wait(5000);
+registrationField.click();
+
+driver.wait(5000);
 
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
     }
 }
+
