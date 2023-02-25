193bbf1dcf85bb5a227c6157059078cc28a1376e
Maximbon77 <104286492+Maximbon77@users.noreply.github.com>
2023-02-24 20:35:40 -0500

    Update LoginTests.java

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -8,6 +8,7 @@ import org.testng.annotations.Test;
 import java.time.Duration;
 
 public class LoginTests extends BaseTest {
+
     public void openLoginUrl(){
         String url = "https://bbb.testpro.io";
         driver.get(url);
@@ -15,31 +16,56 @@ public class LoginTests extends BaseTest {
     public void enterEmail(String email){
         WebElement emailfield = driver.findElement(By.cssSelector("input[type='email']"));
         emailfield.sendKeys(email);
-
     }
     public void enterPassword(String password){
         WebElement passwordfield = driver.findElement(By.cssSelector("input[type='password']"));
         passwordfield.sendKeys(password);
-
     }
     public void submit(){
         WebElement submitbtn = driver.findElement(By.cssSelector("button[type='submit']"));
         submitbtn.click();
     }
-
+    public void login(){
+        enterEmail("maxim.ibon@gmail.com");
+        enterPassword("te$t$tudent1");
+        submit();
+    }
     @Test
-    public void login() {
-
+    public void loginValidCredentials() {
         openLoginUrl();
         enterEmail("maxim.ibon@gmail.com");
         enterPassword("te$t$tudent1");
         submit();
-
         WebElement avatar = driver.findElement(By.cssSelector("img[class='avatar']"));
-        Assert.assertTrue(avatar.isDisplayed());
-        driver.quit();
+        Assert.assertTrue(avatar.isDisplayed(),"Profile avatar is displayed");
     }
+    @Test
+    public void loginInvalidCredentials() {
+        openLoginUrl();
+        enterEmail("maxim.ibon@mail.com");
+        enterPassword("te$t$tuden");
+        submit();
+        WebElement loginform = driver.findElement(By.cssSelector("form[data-testid='login-form']"));
+        Assert.assertTrue(loginform.isDisplayed(),"Login form is Displayed");
+    }
+    @Test
+    public void loginEmailFieldisEmpty() {
+        openLoginUrl();
+        enterEmail("");
+        enterPassword("te$t$tuden1");
+        submit();
+        WebElement loginform = driver.findElement(By.cssSelector("form[data-testid='login-form']"));
+        Assert.assertTrue(loginform.isDisplayed(),"Login form is Displayed");
+    }
+    @Test
+    public void loginPasswordFieldisEmpty() {
+        openLoginUrl();
+        enterEmail("maxim.ibon@mail.com");
+        enterPassword("");
+        submit();
+        WebElement loginform = driver.findElement(By.cssSelector("form[data-testid='login-form']"));
+        Assert.assertTrue(loginform.isDisplayed(),"Login form is Displayed");
 
-
+    }
 
 }
