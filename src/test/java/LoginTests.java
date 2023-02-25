b98c71e17a26b0cb0247450416c5cb0709b2f2ec
amandaregnier <126029067+amandaregnier@users.noreply.github.com>
2023-02-24 17:13:11 -0800

    Update LoginTests.java

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -15,7 +15,7 @@ public class LoginTests extends BaseTest {
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
  //adding a comment for changes
-        String url = "https://bbb.testpro.io/";
+        String url = "https://testpro.io/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
