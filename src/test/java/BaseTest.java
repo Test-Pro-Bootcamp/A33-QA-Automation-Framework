6bb1f355e6fcb7e50663ddd0441036c40acc9aaf
Maximbon77 <104286492+Maximbon77@users.noreply.github.com>
2023-02-24 19:43:05 -0500

    Refactoring

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
@@ -12,16 +13,20 @@ import java.time.Duration;
 
 
 public class BaseTest {
-
+WebDriver driver;
 
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
-/*@AfterSuite
-    static void teardown(){
+    @BeforeMethod
+    public void setupBrowser() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-
+    }
+    @AfterMethod
+    public void teardown(){
         driver.quit();
-}*/
+}
 }
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -8,33 +8,33 @@ import org.testng.annotations.Test;
 import java.time.Duration;
 
 public class LoginTests extends BaseTest {
-    /*
-    WebElement emailfield = driver.findElement(By.cssSelector("input[type='email']"));
-    WebElement passwordfield = driver.findElement(By.cssSelector("input[type='password']"));
-    WebElement submitbtn = driver.findElement(By.cssSelector("button[type='submit']"));
-    public void enterEmail(String email){
-        emailfield.sendKeys(email);
-        return;
-    }
-    public void enterPassword(String password){
-        passwordfield.sendKeys(password);
-        return;
-    }
-    public void submit(){
-        submitbtn.click();
-    }*/
-    @Test
-    public void login() {
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    public void openLoginUrl(){
         String url = "https://bbb.testpro.io";
         driver.get(url);
+    }
+    public void enterEmail(String email){
         WebElement emailfield = driver.findElement(By.cssSelector("input[type='email']"));
+        emailfield.sendKeys(email);
+
+    }
+    public void enterPassword(String password){
         WebElement passwordfield = driver.findElement(By.cssSelector("input[type='password']"));
+        passwordfield.sendKeys(password);
+
+    }
+    public void submit(){
         WebElement submitbtn = driver.findElement(By.cssSelector("button[type='submit']"));
-        emailfield.sendKeys("maxim.ibon@gmail.com");
-        passwordfield.sendKeys("te$t$tudent1");
         submitbtn.click();
+    }
+
+    @Test
+    public void login() {
+
+        openLoginUrl();
+        enterEmail("maxim.ibon@gmail.com");
+        enterPassword("te$t$tudent1");
+        submit();
+
         WebElement avatar = driver.findElement(By.cssSelector("img[class='avatar']"));
         Assert.assertTrue(avatar.isDisplayed());
         driver.quit();
