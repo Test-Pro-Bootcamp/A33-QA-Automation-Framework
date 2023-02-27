e1271571fd02a3353b2bf57ea88ccbbc4eb0bdbf
Lina <angelina.reznichenko@testpro.io>
2023-02-26 22:30:14 -0800

    1

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import java.time.Duration;
 
 
 public class BaseTest {
@@ -14,5 +15,17 @@ public class BaseTest {
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUpBrowser(){
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+    @BeforeMethod
+    public void openloginUrl() {
+        driver.get("https://apps.testpro.io/");
+    }
+
 
 }
---
+++ src/test/java/Homework17.java
@@ -0,0 +1,72 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Homework17 extends BaseTest {
+
+    @Test
+    public void AddSongToPlaylist() throws InterruptedException {
+
+        enterEmail("linulya1411@gmail.com");
+        enterPassword("te$t$tudent");
+        clickSubmit();
+        searchForSong("Beautiful");
+        viewAllResults();
+        selectFirstSong();
+        clickAddTo();
+
+
+
+
+
+
+        //Verify we are still in log in page
+        Thread.sleep(2000);
+
+        Assert.assertEquals(driver.getCurrentUrl(), "https://apps.testpro.io/");
+    }
+
+    public void enterEmail(String email) {
+        WebElement emailField = driver.findElement(By.cssSelector("[type='email'"));
+        emailField.click();
+        emailField.sendKeys(email);
+    }
+
+    public void enterPassword(String password) {
+        WebElement passwordField = driver.findElement(By.cssSelector("[type='password'"));
+        passwordField.click();
+        passwordField.sendKeys(password);
+    }
+
+    public void clickSubmit() {
+        WebElement submitButton = driver.findElement(By.cssSelector("[type='submit'"));
+        submitButton.click();
+    }
+
+    public void searchForSong(String songName) throws InterruptedException {
+        WebElement searchField = driver.findElement(By.cssSelector("#searchForm"));
+        searchField.sendKeys();
+        Thread.sleep(2000);
+    }
+    public void viewAllResults() throws InterruptedException {
+        WebElement viewAllButton = driver.findElement(By.cssSelector("selection#searchExcerptsWrapper section h1 button"));
+        viewAllButton.click();
+        Thread.sleep(2000);
+    }
+    public void selectFirstSong() throws InterruptedException {
+        WebElement selectFirstFromTheList = driver.findElement(By.cssSelector("selection#songResultsWrapper tr.song-item td.title"));
+        selectFirstFromTheList.click();
+        Thread.sleep(2000);
+    }
+    public void clickAddTo() throws InterruptedException {
+        WebElement addToButton = driver.findElement(By.cssSelector("[class='button.btn-add-to']"));
+        addToButton.click();
+        Thread.sleep(2000);
+    }
+
+}
