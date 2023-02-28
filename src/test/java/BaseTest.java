d0c334d5a16fc6a93557aad4657d86b5f3647fc1
janezelenov <118363437+janezelenov@users.noreply.github.com>
2023-02-28 14:10:35 -0500

    Update BaseTest.java

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,4 +1,5 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -13,6 +14,61 @@ public class BaseTest {
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
+
+        mport io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
+
+        public class BaseTest {
+
+            @BeforeSuite
+            static void setupClass() {
+                WebDriverManager.chromedriver().setup();
+            }
+
+            public void enterAllSongs() throw InterruptedException {
+                WebElement allSongs = driver.findElement(By.cssSelector("a[href='#!/songs']"));
+                Thread.sleep(1000);
+                allSongs.click();
+            }
+
+            public void selectSong () throw InterruptedException {
+                WebElement song = driver.findElement(By.xpath("//tr[@class='song-item']"));
+                Thread.sleep(1000);
+                song.click();
+            }
+
+            public void enterButtonPlaySong() throw InterruptedException {
+                WebElement buttonPlaySong = driver.findElement(By.xpath("//span[@title='Play or resume']"));
+                Actions actions = new Actions(driver);
+                actions.click(buttonPlaySong).perform();
+            }
+
+
+            public boolean isDisplayedPlayingSong() {
+
+                WebElement songIsPlaying = driver.findElement(By.cssSelector("[data-testid = 'sound-bar-play']"));
+                return songIsPlaying.isDisplayed();
+
+
+
+            }
+
+        }
+
+
+
+
+
+    }
     }
 
 }
