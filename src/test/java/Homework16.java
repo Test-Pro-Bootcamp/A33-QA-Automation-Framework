d22b7d02dfc5da12b784f1af559f91fdcba7eb7f
JosanAlex <alexandr.josan@yahoo.com>
2023-02-28 19:52:30 -0800

    Update Homework16.java

--- src/test/java/Homework16.java
+++ src/test/java/Homework16.java
@@ -1,11 +1,18 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
-public class Homework16  extends BaseTest{
+import java.time.Duration;
+
+
+public class Homework16 extends BaseTest {
     @Test
-    public void Navigation() {
+    public static void registrationNavigation() {
         WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         String url = "https://bbb.testpro.io/";
         driver.get(url);
