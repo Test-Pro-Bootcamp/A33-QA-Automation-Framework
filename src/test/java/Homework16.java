30d7d6b06dd80660a024d1205b63b34441b5f5cd
arafiqui <ayoub.rafiqui@testpro.io>
2023-03-01 07:06:42 -0800

    Home work 16

--- src/test/java/Homework16.java
+++ src/test/java/Homework16.java
@@ -7,17 +7,20 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class LoginTests extends BaseTest {
+public class Homework16 extends BaseTest {
 
     @Test
-    public static void LoginEmptyEmailPasswordTest() {
+    public static void RegistrationNavigation() {
 
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         String url = "https://bbb.testpro.io/";
+        String url2 = "https://bbb.testpro.io/registration.php";
         driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
+        WebElement RegistrationLink = driver.findElement(By.cssSelector("[id='hel']"));
+        RegistrationLink.click();
+        Assert.assertEquals(driver.getCurrentUrl(), url2);
         driver.quit();
     }
 }
