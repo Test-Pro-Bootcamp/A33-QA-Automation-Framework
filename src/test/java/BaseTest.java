8330eeaf39baeae7e7f46ebe7e2954a7621a7737
janezelenov <118363437+janezelenov@users.noreply.github.com>
2023-03-02 09:56:41 -0500

    Update BaseTest.java

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,6 +1,4 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-<<<<<<< Updated upstream
-=======
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
