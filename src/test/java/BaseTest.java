259d3a22c25cf9649f2fac4b904996b15ca96aa3
janezelenov <118363437+janezelenov@users.noreply.github.com>
2023-03-02 08:25:32 -0500

    Updated homework #18
    It shows me an error: "error: ';' expected
        public void enterAllSongs() throw InterruptedException {
                                   ^"
    How to fix it?
    Thank you 🙏🏼

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -3,12 +3,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
->>>>>>> Stashed changes
 import org.testng.annotations.BeforeSuite;
 
 import java.time.Duration;
@@ -82,6 +79,5 @@ public class BaseTest {
 
 
             }
-
 
 }
