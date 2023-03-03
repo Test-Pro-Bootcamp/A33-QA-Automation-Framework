980a7a333b1d2b82d78e062d0f6aa7b3432b6c28
amandaregnier <126029067+amandaregnier@users.noreply.github.com>
2023-03-03 15:08:29 -0800

    Homework-18
    This isn't running, but I am not sure how to fix it.

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,4 +1,5 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -6,13 +7,32 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
-
+import org.testng.annotations.Test;
 
 public class BaseTest {
-
+    WebDriver driver = new ChromeDriver();
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
 
+    public void playSong() throws InterruptedException {
+        driver.findElement(By.cssSelector("[data-testid='play-next-btn']")).click();
+        driver.findElement(By.cssSelector("[data-testid='play-btn']")).click();
+        WebElement playButton = driver.findElement(By.xpath("//span[@data-testid='play-btn']"));
+        Thread.sleep(5000);
+        playButton.click();
+    }
+    protected void logIn(String email, String password) {
+        WebElement emailField = driver.findElement(By.cssSelector("[type = 'email']"));
+        emailField.sendKeys(email);
+
+        WebElement passwordField = driver.findElement(By.cssSelector("[type = 'password']"));
+        passwordField.sendKeys(password);
+
+        WebElement submitButton = driver.findElement(By.cssSelector("[type = submit']"));
+        submitButton.click();
+
+    }
 }
+
---
+++ src/test/java/Homework18.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.sql.Driver;
+
+
+public class Homework18 extends BaseTest {
+    @Test
+    public void playSongTest() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        logIn("regniermandy@gmail.com", "te$t$tudent");
+        playSong();
+    Assert.assertTrue(isSongPlaying());
+}
+
+    @Test
+    public boolean isSongPlaying() {
+        WebElement soundBarPlay = driver.findElement(By.cssSelector("[data-testid ='sound-bar-play']"));
+        return soundBarPlay.isDisplayed();
+    }
+}
