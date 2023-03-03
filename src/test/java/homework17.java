04c557ad181193bca02b435e3a202150c204e00e
aaronalbusiness <aaronalbusiness@gmail.com>
2023-03-03 07:54:20 -0500

    1 more try

--- src/test/java/homework17.java
+++ src/test/java/homework17.java
@@ -1,3 +1,12 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
 public class homework17 extends Basetest {
 
