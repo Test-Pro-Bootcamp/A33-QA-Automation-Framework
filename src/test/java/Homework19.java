a56ec73a6dd68df19694a750a0e0f84edd96294b
opemipomomoh <125845706+opemipomomoh@users.noreply.github.com>
2023-03-03 22:26:10 -0800

    Homework 19

---
+++ src/test/java/Homework19.java
@@ -0,0 +1,26 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homework19 extends BaseTest {
+
+    @Test
+    public void newSongToPlaylist() throws InterruptedException {
+        setUpBrowser();
+        String url = "https://bbb.testpro.io/";
+        driver.get(url);
+        //getBrowser();
+        emailField("skillztomkid@gmail.com");
+        passwordFinder("te$t$tudent");
+        logIn();
+        searchSongs("dark days");
+        viewAll();
+        clickFirstSong();
+        addToPlaylist();
+        newPlaylist("Ope");
+        opePlaylist();
+        deletePlaylist();
+        notificationIcon();
+        Assert.assertTrue(notificationVerification());
+        //driver.wait(5000);
+    }
+}
