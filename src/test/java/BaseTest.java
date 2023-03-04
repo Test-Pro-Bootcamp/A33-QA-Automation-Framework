f26ec6e30618e85ebd620f305e40cf9bf01dd0f1
Stan8881 <90289447+Stan8881@users.noreply.github.com>
2023-03-04 12:16:00 -0500

    Update BaseTest.java

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,3 +1,11 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -5,11 +13,7 @@ import org.testng.annotations.AfterMethod;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Parameters;
 import org.openqa.selenium.*;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
 import java.time.Duration;
 
 
