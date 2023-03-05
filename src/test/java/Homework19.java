bca8930a76152299043b2f8aa087ece8dc8c3e80
Lina <angelina.reznichenko@testpro.io>
2023-03-05 12:46:03 -0800

    1

---
+++ src/test/java/Homework19.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Homework19 extends BaseTest {
+
+    @Test
+    public static void deletePlayList() {
+    }
+}
