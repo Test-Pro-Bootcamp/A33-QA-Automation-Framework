96d0313fd3ee1832fb446fc479fac52cfde74554
masarich85 <v-mkunihiro@microsoft.com>
2023-03-04 22:10:57 -0800

    Homework18. I couldn't understand the meaning of "Create a new config XML file for the test case".

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,18 +1,58 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.*;
+
+import java.time.Duration;
 
 
 public class BaseTest {
 
+    public static WebDriver driver = null;
+
     @BeforeSuite
-    static void setupClass() {
+    static void setupClass() throws InterruptedException {
         WebDriverManager.chromedriver().setup();
+    }
+    @BeforeMethod
+    public static void launchBrowser() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+
+    @AfterTest
+    public static void closeBrowser(){
+        driver.quit();
+    }
+
+    @Test
+    public static void navigateToPage() throws InterruptedException {
+        String url = "https://bbb.testpro.io/";
+        driver.get(url);
+        Thread.sleep(1000);
+    }
+    public static void provideEmail(String email) {
+        WebElement emailField =driver.findElement(By.cssSelector("[type='email']"));
+        emailField.clear();
+        emailField.sendKeys(email);
+    }
+
+    public static void providePassword(String password) {
+        WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
+        passwordField.click();
+        passwordField.sendKeys(password);
+    }
+
+    public static void clickSubmit() throws InterruptedException {
+        WebElement submitButton = driver.findElement(By.cssSelector("button[type='submit']"));
+        submitButton.click();
+        Thread.sleep(2000);
+
+
     }
 
 }
---
+++ src/test/java/Homework18.java
@@ -0,0 +1,68 @@
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebElement;
+
+    public class Homework18 extends BaseTest {
+  @Test
+  public void playSong() throws InterruptedException {
+
+      setupClass();
+      launchBrowser();
+      navigateToPage();
+      provideEmail("test@test.com");
+      providePassword("te$t$tudent");
+      clickSubmit();
+      Thread.sleep(2000);
+      allSongsPage();
+      clickPlayNextSong();
+      clickPlayBtn();
+      confirmSoundBarIsDisplayed();
+
+
+
+  }
+
+  public void allSongsPage() {
+      WebElement allSongs = driver.findElement(By.cssSelector("a[class='songs']"));
+      allSongs.click();
+  }
+    public void clickPlayNextSong() throws InterruptedException {
+      WebElement playNextBtn = driver.findElement(By.xpath("//footer[@id='mainFooter']/div/i[@role='button'][2]"));
+      playNextBtn.click();
+      Thread.sleep(1000);
+
+    }
+
+    public void clickPlayBtn() {
+        WebElement playMusicBtn = driver.findElement(By.cssSelector("span[class='play']"));
+        playMusicBtn.click();
+
+
+    }
+
+    public boolean confirmSoundBarIsDisplayed() {
+        WebElement soundBarDisplayed = driver.findElement(By.xpath("//div[@class='bars']"));
+        return soundBarDisplayed.isDisplayed();
+    }
+
+
+
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
