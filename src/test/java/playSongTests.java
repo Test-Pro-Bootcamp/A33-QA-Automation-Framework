32c58e04ab55dd22ea51ceb7d8d8b728278101db
janezelenov <118363437+janezelenov@users.noreply.github.com>
2023-03-05 11:29:32 -0500

    Homework #18
    Test passed 👏🏻

--- src/test/java/playSongTests.java
+++ src/test/java/playSongTests.java
@@ -1,14 +1,8 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
 
-
-public class Homework18 extends BaseTest {
+public class playSongTests extends BaseTest {
     @Test
     public void playSong() throws InterruptedException {
         navigateToPage("https://bbb.testpro.io/");
