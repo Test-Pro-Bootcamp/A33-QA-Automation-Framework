d0ba82c8d874d1b36ee0c037a1ca0f6069c2915f
Olesia <sinushkinaolesia@gmail.com>
2023-03-05 10:12:45 -0500

    Homework17

---
+++ src/test/java/Homework17.java
@@ -0,0 +1,23 @@
+import org.testng.annotations.Test;
+
+public class Homework17 extends BaseTest {
+    @Test
+    public void addSongToPlaylist() {
+
+        navigation();
+        loginEmail();
+        loginPassword();
+        submit();
+        search();
+
+
+
+
+
+
+
+    }
+
+
+
+}
