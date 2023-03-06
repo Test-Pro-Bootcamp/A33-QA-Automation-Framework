6d382ff201d7ef13c1c4aca2eb6133c69fa15648
“Katerina <katerina.levchenko@testpro.io>
2023-03-07 08:51:03 +1000

    Homework 20 is done

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,22 +4,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.DataProvider;
 
-import java.security.PublicKey;
 import java.time.Duration;
 
 public class BaseTest {
 
     protected static WebDriver driver  = null;
     protected static WebDriverWait wait;
-    private static int smallWait = 1500;
 
     @BeforeSuite
     protected static void setUpClass () {
@@ -33,59 +29,52 @@ public class BaseTest {
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(1));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(4));
     }
 
     public static void navigateToPage(String url) {
         driver.get(url);
     }
 
-    @DataProvider (name = "Credentials")
-    protected static Object [][] getDataFromDataProvider () {
-        return new Object [][]{
-                {"me@elevchenko.com", "$student1111"},
-        };
-    }
-
-    public static void loginWithValidCredentials (String email, String password) throws InterruptedException {
+    public static void loginWithValidCredentials (String email, String password){
         System.out.println(email);
         System.out.println(password);
-        WebElement emailEl = driver.findElement(By.cssSelector("[type = 'email']"));
+        WebElement emailEl = wait.until(ExpectedConditions.elementToBeClickable((By.cssSelector("[type = 'email']"))));
         emailEl.sendKeys(email);
-        WebElement passwdEl = driver.findElement(By.cssSelector("[type = 'password']"));
+        WebElement passwdEl = wait.until((ExpectedConditions.elementToBeClickable(By.cssSelector("[type = 'password']"))));
         passwdEl.sendKeys(password);
-        driver.findElement(By.cssSelector("button[type = 'submit']")).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[type = 'submit']"))).click();
     }
 
-    protected static void searchSong (String song) throws InterruptedException {
-        WebElement songEl = driver.findElement(By.cssSelector("[type = 'search']"));
+    protected static void searchSong (String song){
+        WebElement songEl = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[type = 'search']")));
         songEl.clear();
         songEl.sendKeys(song);
         String url = driver.getCurrentUrl();
         System.out.println("searchng for " + song + " in " + url);
-        Thread.sleep(2000);
     }
 
-    protected static void viewAllSongs () throws InterruptedException {
+    protected static void viewAllSongs () {
         //view all results for songs
-        WebElement searchResult = driver.findElement(By.cssSelector("section.songs h1 button"));
+        WebElement searchResult = wait.until(ExpectedConditions.presenceOfElementLocated(
+                By.cssSelector("section.songs h1 button")
+        ));
         searchResult.click();
         String url = driver.getCurrentUrl();
         System.out.println("searchng for in " + url);
-        Thread.sleep(1000);
     }
 
-    protected static void selectFirstSong () throws InterruptedException {
-        WebElement songEl = driver.findElement(By.cssSelector("#songResultsWrapper  tr > td.title"));
+    protected static void selectFirstSong () {
+        WebElement songEl = wait.until(ExpectedConditions.elementToBeClickable(
+                By.cssSelector("#songResultsWrapper  tr > td.title")
+        ));
         songEl.click();
-        Thread.sleep(1000);
     }
 
-    protected String clickAddToPls (String playlist) throws InterruptedException{
-        driver.findElement(By.cssSelector("#songResultsWrapper button.btn-add-to")).click();
-        Thread.sleep(2000);
+    protected String clickAddToPls (String playlist) {
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#songResultsWrapper button.btn-add-to"))).click();
         String xpathSelector = "//section[@id='songResultsWrapper']//li[contains(text(),'" + playlist + "')]";
-        WebElement ourPlaylist = driver.findElement(By.xpath(xpathSelector));
+        WebElement ourPlaylist = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpathSelector)));
         ourPlaylist.click();
         /*WebElement playlistEl = driver.findElement(By.cssSelector("#songResultsWrapper section.new-playlist input[type='text']"));
         playlistEl.sendKeys(playlist + "1");
@@ -94,26 +83,29 @@ public class BaseTest {
         String msg1 = driver.findElement(By.cssSelector("div.success.show")).getText();
         Thread.sleep(500);
         System.out.println(msg1);*/
-        String msg = driver.findElement(By.cssSelector("div.success.show")).getText();
+        String msg = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.cssSelector("div.success.show")
+        )).getText();
         System.out.println(msg);
-        Thread.sleep(2000);
         return msg;
     }
 
-    public static void playNextSong () throws  InterruptedException{
-        driver.findElement(By.cssSelector("i[title = 'Play next song']")).click();
-        driver.findElement(By.cssSelector("span.play>i")).click();
-        Thread.sleep(2000);
+    public static void playNextSong () {
+        wait.until(ExpectedConditions.presenceOfElementLocated(
+                By.cssSelector("i[title = 'Play next song']")
+        )).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("span.play>i"))).click();
     }
 
-    public Boolean isPlaying () throws InterruptedException {  //verifying by checking whether the sound bar is displayed
-        Boolean isPlayingFlag = driver.findElement(By.cssSelector("img[alt = 'Sound bars']")).isDisplayed();
-        return isPlayingFlag;
+    public Boolean isPlaying () {  //verifying by checking whether the sound bar is displayed
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.cssSelector("img[alt = 'Sound bars']")
+        )).isDisplayed();
     }
 
-    public static void selectPlaylist (String playlist) throws InterruptedException{
+    public static void selectPlaylist (String playlist){
             String xpathSelector = "//section[@id='playlists']//a[contains(text(),'" + playlist + "')]";
-            WebElement selPlaylist = driver.findElement(By.xpath(xpathSelector));
+            WebElement selPlaylist = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpathSelector)));
             selPlaylist.click();
             System.out.println("Playlist " + playlist + " has been selected");
         /*} catch (Exception e) {
@@ -135,10 +127,11 @@ public class BaseTest {
         }*/
     }
 
-    public static String deleteSelectedPlaylist(String playlist) throws InterruptedException {
-        driver.findElement(By.cssSelector("#playlistWrapper  button.del.btn-delete-playlist")).click();
-        Thread.sleep(smallWait);
-        String msg = driver.findElement(By.cssSelector("div.success.show")).getText();
+    public static String deleteSelectedPlaylist(){
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(
+                "#playlistWrapper  button.del.btn-delete-playlist"
+        ))).click();
+        String msg = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.success.show"))).getText();
         System.out.println("Message is: " + msg);
         return msg;
     }
--- src/test/java/Homework18.java
+++ src/test/java/Homework18.java
@@ -1,18 +1,10 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class Homework18 extends BaseTest {
-//    Create a test case, playSong() using @Test annotation.
-//    Use the helper/reusable methods we created.
-//    Create a new config XML file for the test case.
-//    Specify the method to be ran in the XML file.
-//    Navigate to "https://bbb.testpro.io/".
-//    Log in with your credentials.
-//    Click «Play next song» (media player controls), then the Play button, to play a song.
-//    Validate that a song is playing by verifying if the sound bar, or the pause button is displayed.
+public class Homework18 extends BaseTest{
 
     @Test
-    public void playSong () throws InterruptedException {
+    public void playSong (){
 
         //variables
         String homeUrl = "https://bbb.testpro.io/";
