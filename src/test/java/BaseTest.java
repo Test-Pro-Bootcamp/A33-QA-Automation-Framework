d48ce673884d6ba0d9413b21ba663eb112229489
RuthA23 <ruth.asghar@testpro.io>
2023-03-08 17:15:33 -0600

    changes

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,12 +1,80 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
-
-
-public class BaseTest {
-
+    public class BaseTest {
+    public static WebDriver driver = null;
+    String url = "https://bbb.testpro.io/";
+    By emailField = By.cssSelector("[type = 'email']");
+    By passwordField = By.cssSelector("[type = 'password']");
+    By submitButton = By.cssSelector("[type = 'submit']");
+    By searchField = By.cssSelector("[type='search']");
+    By viewAllButton = By.cssSelector("[data-test ='view-all-songs-btn']");
+    By selectSong = By.cssSelector("section#songResultsWrapper tr.song-item td.title");
+    By addToButton = By.cssSelector("[data-test = 'add-to-btn']");
+    By choosePlaylist = By.xpath("//section[@id='songResultsWrapper']//li[contains(text(), 'Musical')]");
+    By notification = By.cssSelector("div.success.show");
     @BeforeSuite
     static void setupClass() {
-        WebDriverManager.chromedriver().setup();
+    WebDriverManager.chromedriver().setup();
     }
-
+    @BeforeMethod
+    public void setUpBrowser() {
+    driver = new ChromeDriver();
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+    }
+    @AfterMethod
+    public void tearDownBrowser() {
+    driver.quit();
+    }
+    public void openLoginURL() {
+    driver.get(url);
+    }
+    public void provideEmail(String email) {
+    WebElement emailElement = driver.findElement(emailField);
+    emailElement.sendKeys(email);
+    }
+    public void providePW(String password) {
+    WebElement passwordField = driver.findElement(passwordField);
+    passwordField.sendKeys(password);
+    }
+    public void clickSubmit() throws InterruptedException {
+    WebElement subButton = driver.findElement(submitButton);
+    subButton.click();
+     Thread.sleep(1000);
+    }
+    public void searchSong(String songText) throws InterruptedException {
+    WebElement searchElement = driver.findElement(searchField);
+    searchElement.sendKeys(songText);
+    Thread.sleep(1000);
+    }
+    public void clickViewAllButton() throws InterruptedException {
+    WebElement viewAllElement = driver.findElement(viewAllButton);
+    viewAllElement.click();
+    Thread.sleep(1000);
+    }
+    public void clickFirstSong() throws InterruptedException {
+    WebElement clickSong = driver.findElement(selectSong);
+    clickSong.click();
+    Thread.sleep(1000);
+    }
+    public void clickAddToButton() throws InterruptedException {
+    WebElement addToElement = driver.findElement(addToButton);
+    addToElement.click();
+    Thread.sleep(1000);
+    }
+    public void clickPlaylist() throws InterruptedException {
+    WebElement choosePlaylistElement = driver.findElement(choosePlaylist);
+    choosePlaylistElement.click();
+    Thread.sleep(1000);
+    }
+    public String getNotification() {
+    WebElement notificationMessage = driver.findElement(notification);
+    return notificationMessage.getText();
+    }
 }
+
+
+
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -112,3 +112,19 @@ public class LoginTests extends BaseTest {
         driver.quit();
     }
 }
+    public class RAHomework17 extends BaseTest {
+    @Test
+    public void addSongToPlaylist() throws InterruptedException {
+        String notificationText = "Added 1 song into";
+        openLoginURL();
+        provideEmail("ruth.asghar@testpro.io");
+        providePW("te$t$tudent");
+        clickSubmit();
+        searchSong("Take my Hand");
+        clickViewAllButton();
+        clickFirstSong();
+        clickAddToButton();
+        clickPlaylist();
+        Assert.assertTrue(getNotification().contains(notificationText));
+    }
+}
