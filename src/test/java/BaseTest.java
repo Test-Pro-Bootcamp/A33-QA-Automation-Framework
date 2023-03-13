ea97090a6db5e9c63b57228733c466630a29864c
Medina559 <lamas0411@gmail.com>
2023-03-13 02:55:29 -0700

    HW17 submission - Guadalupe

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,12 +1,61 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+
+import java.time.Duration;
 
 
 public class BaseTest {
+
+    public static WebDriver driver= null;
 
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
 
+    @BeforeMethod
+    public static void launchBrowser() {
+//       Chromeoptions argument fixes the error below:
+        //WARNING: Invalid Status code=403 text=Forbidden
+        //java.io.IOException: Invalid Status code=403 text=Forbidden
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @AfterMethod
+    public static void closeBrowser() {
+        driver.quit();
+    }
+
+    protected static void navigateToPage() {
+        String url = "https://bbb.testpro.io/";
+        driver.get(url);
+    }
+
+    public static void provideEmail(String email) {
+        WebElement emailField = driver.findElement(By.cssSelector("[type ='email']"));
+        emailField.clear();
+        emailField.sendKeys(email);
+    }
+
+    public static void passwordField(String password) {
+        WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
+        passwordField.clear();
+        passwordField.sendKeys(password);
+    }
+
+    public static void submitButton() throws InterruptedException {
+        WebElement submitButton = driver.findElement(By.cssSelector("Button[type ='submit']"));
+        submitButton.click();
+        Thread.sleep(2000);
+    }
 }
---
+++ src/test/java/HomeWork17.java
@@ -0,0 +1,68 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+
+
+    public class HomeWork17 extends BaseTest {
+
+        @Test
+        public void addSongToPlaylist() throws InterruptedException {
+
+            String newSongAddedNotificationText = "Add 1 song into";
+
+
+            navigateToPage();
+            provideEmail("guadalupe.medina@testpro.io");
+            passwordField("DoingitBig23!");
+            submitButton();
+            searchSong("BossStatus");
+            clickViewAllBtn();
+            selectFirstSongResults();
+            clickAddToBtn();
+            choosePlaylist("JAMS");
+
+            Assert.assertTrue(isNotificationPopUpPresent());
+        }
+
+    public void searchSong(String songTitleKeyword) throws InterruptedException {
+        WebElement searchField = driver.findElement(By.cssSelector("div#searchForm input[type=search]"));
+        searchField.sendKeys(songTitleKeyword);
+        Thread.sleep(2000);
+    }
+
+    public void clickViewAllBtn() throws InterruptedException {
+        WebElement clickViewAllBtn = driver.findElement(By.cssSelector("div.results section.songs h1 button"));
+        clickViewAllBtn.click();
+        Thread.sleep(2000);
+    }
+
+    public void selectFirstSongResults() throws InterruptedException {
+        WebElement selectFirstSongResults = driver.findElement(By.cssSelector("section#songResultsWrapper tr.song-item td.title"));
+        selectFirstSongResults.click();
+        Thread.sleep(2000);
+    }
+
+    public void clickAddToBtn() throws InterruptedException {
+        WebElement addTo = driver.findElement(By.cssSelector("button.btn-add-to"));
+        addTo.click();
+        Thread.sleep(2000);
+    }
+
+//    public void createNewPlaylist() throws InterruptedException {
+        public void choosePlaylist(String playlistName) throws InterruptedException {
+            WebElement playlistNameElement = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//section/ul/li[contains(text(),'"+playlistName+"')]"));
+        playlistNameElement.click();
+        Thread.sleep(2000);
+    }
+
+
+    public boolean isNotificationPopUpPresent() {
+        WebElement newSongAddedNotificationText = driver.findElement(By.cssSelector("div.success.show"));
+        return newSongAddedNotificationText.isDisplayed();
+    }
+}
+
+
