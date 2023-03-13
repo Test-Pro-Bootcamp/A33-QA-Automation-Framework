00944485d7b7625ba07ea51790d0b6f56c8945fe
niaandalia <126042724+niaandalia@users.noreply.github.com>
2023-03-13 16:56:23 -0700

    Homework20

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,22 +1,22 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Parameters;
 
-import java.sql.Driver;
 import java.time.Duration;
 
 
 public class BaseTest {
-    static WebDriver driver;
-    String url;
+   public static WebDriver driver = null;
+   public static String url = null;
+   public static WebDriverWait wait = null;
   
     @BeforeSuite
     static void setupClass() {
@@ -24,32 +24,78 @@ public class BaseTest {
     }
     @BeforeMethod
     @Parameters({"baseUrl"})
-    public void setUpBrowser(String baseUrl){
+    public void launchBrowser(String baseUrl){
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         driver.manage().window().maximize();
-        url=baseUrl;
+        url = baseUrl;
         driver.get("url");
         
     }
 
-
-    public void logIn(String email, String password){
-     WebElement emailField = driver.findElement(By.cssSelector("[type = 'email']"));
+    public static void provideEmail(String email){
+     WebElement emailField = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[type='email']")));
      emailField.click();
      emailField.sendKeys(email);
-     WebElement passwordField = driver.findElement(By.cssSelector("[type = 'password']"));
-     passwordField.sendKeys(password);
-
+     
     }
-      public static void clickSubmit() throws InterruptedException{
-        WebElement submitButton = driver.findElement(By.cssSelector("[type='submit']"));
-        submitButton.click();
-        Thread.sleep(2000);
+      public static void providePassword(String password){
+        WebElement passwordField = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[type='email']")));
+        passwordField.click();
+        passwordField.sendKeys(password);
       }
+    
+      public static void clickSubmit(){
+        WebElement submitButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[type='submit']")));
+        submitButton.click();
+  
+      }
+      public static void logIn(String email, String password){
+        provideEmail(email);
+        providePassword(password);
+        clickSubmit();
+        
+      }
+        public static void createNewPlaylist(){
+        WebElement newPlaylistIcon = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("i[title='create new playlist']")));
+        newPlaylistIcon.click();
+        WebElement newPlayList = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("li[data-testid='playlist-context-menu-create-simple']")));
+        newPlayList.click();
+        WebElement nameField = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("input[name='name']")));
+        nameField.clear();
+        nameField.sendKeys("Pluto");
+        nameField.sendKeys(Keys.ENTER);
+        }
+
+        public static boolean newPlaylistDisplayed(){
+        WebElement newPlaylist = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("a[class='active']")));
+        return newPlaylistDisplayed();
+        }
+
+        public static void searchSong(String songName){
+        WebElement searchSong = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[type='search']")));
+        searchSong.sendKeys(songName);
+        
+        }
+        public static void clickViewAllButton(){
+        WebElement clickViewAllButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[data-test = 'view-all-songs-btn']")));
+        clickViewAllButton.click();
+        }
+
+        public static void selectFirstSong(){
+        WebElement selectFirstSong = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("section#songResultsWrapper tr.song-item td.title")));
+        selectFirstSong.click();
+        }
+
+        public static void clickAddToButton(){
+         WebElement addToButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[class = 'btn-add-to'")));
+         addToButton.click();
+        }
+
+
     public WebElement getDeletedPlaylistMsg(){
         return driver.findElement(By.cssSelector("div.success.show"));
-        
+
     }
     public void openPlaylist() throws InterruptedException{
         WebElement emptyPlaylist = driver.findElement(By.cssSelector(".playlist:nth-child(3)"));
---
+++ src/test/java/Homework20.java
@@ -0,0 +1,23 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homework20 extends BaseTest {
+
+    @Test
+    public static void createNewPlaylistTest() {
+        logIn("andalia.nia@gmail.com", "te$t$tudent");
+        clickSubmit();
+        createNewPlaylist();
+        Assert.assertTrue(newPlaylistDisplayed());
+    }
+    @Test
+    public static void addASongToPlaylist(){
+        logIn("andalia.nia@gmail.com","te$t$tudent");
+        clickSubmit();
+        searchSong("Pluto");
+        clickViewAllButton();
+        selectFirstSong();
+        clickAddToButton();
+        Assert.assertTrue(newPlaylistDisplayed());
+    }
+}
