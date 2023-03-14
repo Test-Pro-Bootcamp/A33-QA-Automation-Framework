2e9127ecdd27fcbb56fff7a7f3e06a5d178d2f4b
ElenaNicoara <125710494+ElenaNicoara@users.noreply.github.com>
2023-03-14 17:23:51 -0400

    Homework23

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -4,102 +4,133 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.Test;
+import org.testng.IExpectedExceptionsHolder;
 
 import java.time.Duration;
 
 public class HomePage extends BasePage {
-   WebDriver driver;
+  static WebDriver driver;
    static WebDriverWait wait;
    String playlistName = "Sleepy Songs";
+
    private By userAvatarIcon = By.cssSelector("img.avatar");
-   private By newPlaylistIcon = By.cssSelector("i[title='Create a new playlist']");
-   private By newPlaylist = By.cssSelector("li[data-testid='playlist-context-menu-create-simple']");
-   private By nameField = By.cssSelector("input[name='name']");
-   private By searchSong = By.cssSelector("[type='search'");
-   private By clickViewAllBtn = By.cssSelector("button[data-test = 'view-all-songs-btn']");
-   private By firstSong = By.cssSelector("section#songResultsWrapper tr.song-item td.title");
-   private By addToButton = By.cssSelector("button[class = 'btn-add-to']");
-   private By clickPlaylistNameFromAddToBtn = By.xpath
-           ("//section[@id='songResultsWrapper']//section/ul//li[contains(text(), ' " + playlistName + "')]");
-   private By selectPlaylist = By.cssSelector("li[class='playlist playlist'");
-   private By selectTheSong = By.xpath("//*[@id='playlistWrapper']/div/div/div[1]/table/tr/td[2]");
-   private By deleteSong = By.xpath("//*[@id= 'playlistWrapper']/div/div/div[1]/table/tr");
-   private By clickPlaylist = By.xpath("//a[contains(text(), 'Sleepy Songs')]");
-   private By clickXPlaylistButton = By.cssSelector("button[title='Delete this playlist'");
-   private By clickExistingPlaylist = By.cssSelector(".playlist:nth-child(3)");
-   private By choosePlaylist = By.xpath("//a[contains(text(),'Active Songs')]");
-   private By editButton = By.cssSelector("#playlists > ul > li:nth-child(3) > nav > ul > li:nth-child(1)");
-   private By enterPlaylistNames = By.cssSelector("input[name='name']");
+   @FindBy(css = "i[title='Create a new playlist']")
+   private WebElement newPlaylistIcon;
+   @FindBy(css = "li[data-testid='playlist-context-menu-create-simple']")
+   private WebElement newPlaylist;
+   @FindBy(css = "input[name='name']")
+   private WebElement nameField;
+   @FindBy(css = "[type='search'")
+   private WebElement searchSong;
+   @FindBy(css = "button[data-test = 'view-all-songs-btn']")
+   private WebElement clickViewAllBtn;
+   @FindBy(css = "section#songResultsWrapper tr.song-item td.title")
+   private WebElement firstSong;
+   @FindBy(css = "button[class = 'btn-add-to']")
+   private WebElement addToButton;
+   @FindBy(css = "li[class='playlist playlist'")
+   private WebElement selectPlaylist;
+   @FindBy(xpath = "//*[@id='playlistWrapper']/div/div/div[1]/table/tr/td[2]")
+   private WebElement selectTheSong;
+   @FindBy(xpath = "//*[@id= 'playlistWrapper']/div/div/div[1]/table/tr")
+   private WebElement deleteSong;
+   @FindBy(xpath = "//a[contains(text(), 'Sleepy Songs')]")
+   private WebElement clickPlaylist;
+   @FindBy(css = "button[title='Delete this playlist'")
+   private WebElement clickPlaylistButton;
+   @FindBy(css = "button[title='Delete this playlist'")
+   private WebElement clickXPlaylistButton;
+   @FindBy(css = ".playlist:nth-child(3)")
+   private WebElement clickExistingPlaylist;
+   @FindBy(xpath = "//a[contains(text(),'Active Songs')]")
+   private WebElement choosePlaylist;
+   @FindBy(css = "#playlists > ul > li:nth-child(3) > nav > ul > li:nth-child(1)")
+   private WebElement editButton;
+   @FindBy(css = "input[name='name']")
+   private WebElement enterPlaylistNames;
+   @FindBy(xpath = "//*[@id= 'songResultsWrapper']/header/div[3]/div/section[1]/ul/li[5]")
+   private WebElement clickPlaylistNameFromAddBtn;
    public HomePage(WebDriver givenDriver){ super(givenDriver);
       driver = givenDriver;
       wait = new WebDriverWait(driver, Duration.ofSeconds(10));
    }
    public WebElement getUserAvatar(){
-      return wait.until(ExpectedConditions.visibilityOfElementLocated
-              (userAvatarIcon));
+      return findElement(userAvatarIcon);
    }
-   public void createPlaylist(){
-      wait.until(ExpectedConditions.elementToBeClickable(newPlaylistIcon)).click();
-      wait.until(ExpectedConditions.visibilityOfElementLocated(newPlaylist)).click();
-      wait.until(ExpectedConditions.elementToBeClickable(nameField)).sendKeys
-              ((Keys.chord(playlistName, Keys.ENTER)));
+   public HomePage createPlaylist(){
+      wait.until(ExpectedConditions.visibilityOf(newPlaylistIcon)).click();
+      wait.until(ExpectedConditions.visibilityOf(newPlaylist)).click();
+      nameField.sendKeys((Keys.chord(playlistName, Keys.ENTER)));
+      return this;
    }
    public static boolean newPlaylistIsDisplayed(){
-      WebElement newPlaylist = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector
+      WebElement newPlaylist = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector
               ("a[class='active']")));
       return newPlaylist.isDisplayed();
    }
-   public void searchASong(String songName){
-      wait.until(ExpectedConditions.visibilityOfElementLocated(searchSong)).sendKeys(songName);
+   public HomePage searchASong(String songName){
+      wait.until(ExpectedConditions.visibilityOf(searchSong)).sendKeys(songName);
+      return this;
    }
-   public void clickViewAllButton(){
-      wait.until(ExpectedConditions.elementToBeClickable(clickViewAllBtn)).click();
+   public HomePage clickViewAllButton(){
+      wait.until(ExpectedConditions.visibilityOf(clickViewAllBtn)).click();
+      return this;
    }
-   public void selectFirstSong(){
-      wait.until(ExpectedConditions.visibilityOfElementLocated(firstSong)).click();
+   public HomePage selectFirstSong(){
+      wait.until(ExpectedConditions.visibilityOf(firstSong)).click();
+      return this;
    }
-   public void clickAddToButton(){
-      wait.until(ExpectedConditions.elementToBeClickable(addToButton)).click();
+   public HomePage clickAddToButton(){
+      wait.until(ExpectedConditions.visibilityOf(addToButton)).click();
+      return this;
    }
-   public void clickPlaylistNameFromAddToButton(String playlistName){
-      wait.until(ExpectedConditions.elementToBeClickable(clickPlaylistNameFromAddToBtn)).click();
-   }
-   public static boolean notificationMessageIsDisplayed(){
-      WebElement getNotificationMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector
-              ("div.success.show")));
-      return getNotificationMessage.isDisplayed();
-   }
-   public void deleteSongFromPlaylist(){
-      wait.until(ExpectedConditions.visibilityOfElementLocated(selectPlaylist)).click();
-      wait.until(ExpectedConditions.elementToBeClickable(selectTheSong)).click();
-      wait.until(ExpectedConditions.elementToBeClickable(deleteSong)).click();
+   public HomePage clickPlaylistNameFromAddToButton(String playlistName){
+       wait.until(ExpectedConditions.visibilityOf(clickPlaylistNameFromAddBtn));
+      clickPlaylistNameFromAddBtn.click();
+      return this;
+      }
+    public static boolean notificationMessageIsDisplayed(){
+        WebElement getNotificationMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector
+                ("div.success.show")));
+        return getNotificationMessage.isDisplayed();
+    }
+
+   public HomePage deleteSongFromPlaylist(){
+      wait.until(ExpectedConditions.visibilityOf(selectPlaylist)).click();
+      wait.until(ExpectedConditions.visibilityOf(selectTheSong)).click();
+      wait.until(ExpectedConditions.visibilityOf(deleteSong)).click();
       actions.sendKeys(Keys.DELETE).perform();
+      return this;
    }
-   public void clickUpdatedPlaylist(){
-      wait.until(ExpectedConditions.elementToBeClickable(choosePlaylist)).click();
+   public HomePage clickUpdatedPlaylist(){
+      wait.until(ExpectedConditions.visibilityOf(choosePlaylist)).click();
+      return this;
    }
-   public void deleteExistingPlaylist(){
-      wait.until(ExpectedConditions.elementToBeClickable(clickXPlaylistButton)).click();
+   public HomePage deleteExistingPlaylist(){
+      wait.until(ExpectedConditions.visibilityOf(clickXPlaylistButton)).click();
+      return this;
    }
    public static boolean getNotificationMessage(){
       WebElement notificationMessage = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector
               ("div.success.show")));
       return notificationMessage.isDisplayed();
    }
-   public void contextClickExistingPlaylist(){
-      contextClick(clickExistingPlaylist);
+   public HomePage contextClickExistingPlaylist(){
+      WebElement existingPlaylist = wait.until(ExpectedConditions.visibilityOf(clickExistingPlaylist));
+      actions.contextClick(existingPlaylist).perform();
+      return this;
    }
-   public void chooseEdit(){
-      wait.until(ExpectedConditions.elementToBeClickable(editButton)).click();
+   public HomePage chooseEdit(){
+      editButton.click();
+      return this;
    }
-   public void renamePlaylist(String newPlaylistName){
-      WebElement enterPlaylistName = wait.until(ExpectedConditions.elementToBeClickable(enterPlaylistNames));
-       enterPlaylistName.sendKeys((Keys.chord(Keys.CONTROL + "a")));
-       enterPlaylistName.sendKeys(newPlaylistName);
-       enterPlaylistName.sendKeys(Keys.ENTER);
+   public HomePage renamePlaylist(String newPlaylistName){
+      wait.until(ExpectedConditions.visibilityOf(enterPlaylistNames));
+      enterPlaylistNames.sendKeys((Keys.chord(Keys.CONTROL + "a",Keys.BACK_SPACE)),newPlaylistName,Keys.ENTER);
+      return this;
    }
    public static boolean displayUpdatedPlaylist(String newPlaylistName){
       WebElement updatedPlaylist = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath
--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -3,20 +3,32 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 public class LoginPage extends BasePage{
-    private By emailFieldInput = By.cssSelector("[type = 'email']");
-    private By passwordFieldInput = By.cssSelector("[type = 'password']");
-    private By submitButton = By.cssSelector("button[type = 'submit']");
-
+    @FindBy(css = "[type = 'email']")
+    private WebElement emailFieldInput;
+    @FindBy(css = "[type = 'password']")
+    private WebElement passwordFieldInput;
+    @FindBy(css = "button[type = 'submit']")
+    private WebElement submitButton;
     public LoginPage(WebDriver givenDriver){super(givenDriver);}
 
-    public void provideEmail (String email){driver.findElement(emailFieldInput).sendKeys(email);}
-    public void providePassword (String password){driver.findElement(passwordFieldInput).sendKeys(password);}
-    public void clickSubmitButton(){driver.findElement(submitButton).click();}
+    public LoginPage provideEmail (String email){
+        emailFieldInput.sendKeys(email);
+        return this;
+    }
+    public LoginPage providePassword (String password){
+        passwordFieldInput.sendKeys(password);
+        return this;
+    }
+    public LoginPage clickSubmitButton(){
+        submitButton.click();
+        return this;
+    }
     public void login(){
         provideEmail("nelena35@yahoo.com");
         providePassword("te$t$tudent");
