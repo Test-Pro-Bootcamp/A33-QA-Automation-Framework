2e19b18fb7abf8551a22fb96610dcff1787e4c21
janezelenov <118363437+janezelenov@users.noreply.github.com>
2023-03-15 08:31:31 -0400

    Homework#23

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -3,22 +3,40 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class LoginPage extends BasePage {
 
+    @FindBy(css = "[type='submit']")
+    static
+    WebElement submitButton;
+
+    @FindBy(css="[type='email']")
+    static
+    WebElement emailField;
+
+    @FindBy(css="type='password']")
+    static
+    WebElement passwordField;
+
+
+
     public static void provideEmail(String email) {
-        WebElement emailField = driver.findElement(By.cssSelector("[type = 'email']"));
+        //WebElement emailField = driver.findElement(By.cssSelector("[type = 'email']"));
         emailField.sendKeys(email);
+        return this;
     }
 
     public static void providePassword(String password) {
-        WebElement passwordField = driver.findElement(By.cssSelector("[type = 'password']"));
+        //WebElement passwordField = driver.findElement(By.cssSelector("[type = 'password']"));
         passwordField.sendKeys(password);
+        return this;
     }
 
     public static void clickSubmit() {
-        WebElement submitButton = driver.findElement(By.cssSelector("button[type = 'submit']"));
+        //WebElement submitButton = driver.findElement(By.cssSelector("button[type = 'submit']"));
         submitButton.click();
+        return this;
     }
 
     public void logIn() {
