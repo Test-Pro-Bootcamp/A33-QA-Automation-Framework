d5fa57da753805eebe40684e750b42aa423546a4
darkpool77 <darkpool77@users.noreply.github.com>
2023-03-15 20:11:32 -0700

    HomeWork23

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -2,7 +2,6 @@ package Pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
 
 public class LoginPage  extends BasePage{
@@ -15,15 +14,6 @@ public class LoginPage  extends BasePage{
     @FindBy(css="[type='password']")
     WebElement passwordField;
 
-    @FindAll({
-            @FindBy(id = "books-wrapper"),
-            @FindBy(id = "userName-value]"),
-    })
-    WebElement xyz;
-
-//    By emailFieldLocator = By.cssSelector("[type='email']");
-//    By passwordFieldLocator = By.cssSelector("[type='password']");
-//    By submitButtonLocator = By.cssSelector("[type='submit']");
 
     public LoginPage(WebDriver givenDriver){
         super(givenDriver);
@@ -33,14 +23,11 @@ public class LoginPage  extends BasePage{
     public LoginPage provideEmail(String email){
         emailField.sendKeys(email);
         return this;
-
-        //driver.findElement(emailFieldLocator).sendKeys(email);
     }
 
     public LoginPage providePassword(String password){
         passwordField.sendKeys(password);
         return this;
-        //driver.findElement(passwordFieldLocator).sendKeys(password);
     }
 
     public LoginPage clicksubmit() {
@@ -50,12 +37,7 @@ public class LoginPage  extends BasePage{
 
 
     public void login(){
-//        provideEmail("valentineguts@gmail.com");
-//        providePassword("te$t$tudent");
-//        clicksubmit();
-
         provideEmail("valentineguts@gmail.com").providePassword("te$t$tudent").clicksubmit();
-//    this method (fluent) works only if with a return inside the method we are calling;
 
     }
 
