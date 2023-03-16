2d203dd876f6f9669efd0ac0a5a6f65306e5fcc0
veradamp <126361078+veradamp@users.noreply.github.com>
2023-03-15 23:44:13 -0400

    Homework18

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,18 +1,73 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
-
+import org.openqa.selenium.*;
+import org.testng.Assert;
+import java.time.Duration;
 
 public class BaseTest {
+    public static WebDriver driver = null;
 
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
 
+    @BeforeMethod
+    public static void setUpBrowser() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @AfterMethod
+    public static void closeDownBrowser() {
+        driver.quit();
+    }
+
+    public void openLoginUrl() {
+        String url = "https://bbb.testpro.io/";
+        driver.get(url);
+    }
+
+    public static void provideEmail(String email) {
+        WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
+        emailField.click();
+        emailField.sendKeys(email);
+    }
+
+    public static void enterPassword(String password) {
+        WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
+        passwordField.click();
+        passwordField.sendKeys(password);
+    }
+
+    public static void clickSubmit() throws InterruptedException {
+        WebElement loginButton = driver.findElement(By.cssSelector("[type='submit']"));
+        loginButton.click();
+        Thread.sleep(2000);
+    }
+
+    public void playNextSong() throws InterruptedException {
+        WebElement buttonPlayNextSong = driver.findElement(By.cssSelector("[data-testid='play-next-btn']"));
+        Thread.sleep(5000);
+        buttonPlayNextSong.click();
+    }
+
+    public void playSong() throws InterruptedException {
+        WebElement buttonPlaySong = driver.findElement(By.cssSelector("[data-testid='play-btn']"));
+        Thread.sleep(5000);
+        buttonPlaySong.click();
+    }
+
+    public boolean isDisplayedPlayingSong() {
+        WebElement songIsPlaying = driver.findElement(By.cssSelector("[data-testid='sound-bar-play']"));
+        return songIsPlaying.isDisplayed();
+    }
 }
---
+++ src/test/java/Homework18.java
@@ -0,0 +1,22 @@
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import java.time.Duration;
+public class Homework18 extends BaseTest {
+    @Test
+    public void playASong() throws InterruptedException {
+
+
+        openLoginUrl();
+        provideEmail("vera1077@gmail.com");
+        enterPassword("te$t$tudent");
+        clickSubmit();
+        playNextSong();
+        playSong();
+        Assert.assertTrue(isDisplayedPlayingSong());
+    }
+}
