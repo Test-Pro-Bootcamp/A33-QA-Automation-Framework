2b3a3f8af0f40cda6c76ae424e8e284b3a2dd7a5
ElenaNicoara <125710494+ElenaNicoara@users.noreply.github.com>
2023-03-18 01:22:07 -0400

    Homework25

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,18 +1,103 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
+import org.openqa.selenium.safari.SafariOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.*;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.time.Duration;
+import java.util.HashMap;
 
 
 public class BaseTest {
+    public static WebDriver driver = null;
+    public static ThreadLocal<WebDriver> threadDriver;
+    public static WebDriverWait wait = null;
+    public static String url = null;
+    public static Actions actions = null;
 
     @BeforeSuite
-    static void setupClass() {
+    public void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
+    @BeforeMethod
+    @Parameters({"baseUrl"})
+    public void launchBrowser(@Optional String baseUrl) throws MalformedURLException {
+        threadDriver = new ThreadLocal<>();
+        driver = pickBrowser(System.getProperty("browser"));
+//        driver = pickBrowser("cloud");
+        threadDriver.set(driver);
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
+        getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        getDriver().manage().window().maximize();
+        actions = new Actions(getDriver());
+        url = baseUrl;
+        getDriver().get(url);
+    }
+    public static WebDriver lambdaTest() throws MalformedURLException {
+        String hubURL = "https://hub.lambdatest.com/wd/hub";
 
+        ChromeOptions browserOptions = new ChromeOptions();
+        browserOptions.setPlatformName("Windows 10");
+        browserOptions.setBrowserVersion("110.0");
+        HashMap<String, Object> ltOptions = new HashMap<String, Object>();
+        ltOptions.put("username", "nelena35");
+        ltOptions.put("accessKey", "6D9yzrrfh3h2EPqM7PBxkivvaSa3f20suqAfbajUojTSreJV36");
+        ltOptions.put("project", "Untitled");
+        ltOptions.put("selenium_version", "4.0.0");
+        ltOptions.put("w3c", true);
+        browserOptions.setCapability("LT:Options", ltOptions);
+
+        return new RemoteWebDriver(new URL(hubURL), browserOptions);
+    }
+    private static WebDriver pickBrowser(String browser) throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        String gridURL = "http://192.168.0.115:4444";
+        switch (browser) {
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                return driver = new FirefoxDriver();
+            case "MicrosoftEdge":
+                WebDriverManager.edgedriver().setup();
+                return driver = new EdgeDriver();
+            case "grid-firefox":
+                caps.setCapability("browserName", "firefox");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
+            case "grid-edge":
+                caps.setCapability("browserName", "MicrosoftEdge");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
+            case "grid-chrome":
+                caps.setCapability("browserName", "chrome");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
+            case "cloud":
+                return lambdaTest();
+            default:
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--remote-allow-origins=*","--disable-notifications","--incognito", "--start-maximized");
+                return driver = new ChromeDriver(options);
+        }
+    }
+    @AfterMethod
+    public static void closeBrowser(){
+        getDriver().quit();
+        threadDriver.remove();
+    }
+    public static WebDriver getDriver(){
+        return threadDriver.get();
+    }
 }
+
---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,38 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class LoginPage extends BasePage{
+    @FindBy(css = "[type = 'email']")
+    private WebElement emailFieldInput;
+    @FindBy(css = "[type = 'password']")
+    private WebElement passwordFieldInput;
+    @FindBy(css = "button[type = 'submit']")
+    private WebElement submitButton;
+    public LoginPage(WebDriver givenDriver){super(givenDriver);}
+
+    public LoginPage provideEmail (String email){
+        emailFieldInput.sendKeys(email);
+        return this;
+    }
+    public LoginPage providePassword (String password){
+        passwordFieldInput.sendKeys(password);
+        return this;
+    }
+    public LoginPage clickSubmitButton(){
+        submitButton.click();
+        return this;
+    }
+    public void login(){
+        provideEmail("nelena35@yahoo.com");
+        providePassword("te$t$tudent");
+        clickSubmitButton();
+    }
+}
