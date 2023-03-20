f5908ce296a6eb755cfee3b6ffe3fcd9d04353f3
Mohamed Taqi <126124586+MedTaqi@users.noreply.github.com>
2023-03-20 01:12:29 -0400

    Homework23

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,12 +1,45 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
+
+import java.time.Duration;
 
 
 public class BaseTest {
+    public static WebDriver driver = null;
+    public static WebDriverWait wait = null;
+    public static String url = null;
+    public static Actions actions = null;
 
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
 
+    @BeforeMethod
+    @Parameters({"baseUrl"})
+    public static void launchBrowser(String baseUrl) {
+        ChromeOptions ops = new ChromeOptions();
+        ops.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(ops);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        actions = new Actions(driver);
+        url = baseUrl;
+        driver.get(url);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+    }
+
+    @AfterMethod
+    public static void closeBrowser() {
+        driver.quit();
+    }
 }
+
---
+++ src/test/java/Pages/BasePage.java
@@ -0,0 +1,29 @@
+package Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BasePage {
+    protected static WebDriverWait wait;
+    protected Actions actions;
+    protected WebDriver driver;
+
+    public BasePage(WebDriver givenDriver) {
+        driver = givenDriver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        actions = new Actions(driver);
+        PageFactory.initElements(driver,this);
+    }
+
+    public void contextClick(By locator) {
+        WebElement clickElement = wait.until(ExpectedConditions.elementToBeClickable(locator));
+        actions.contextClick(clickElement).perform();
+    }
+}
