2737597b8a161d1cc1990b663d9dfc84f3f1fd74
ElenaNicoara <125710494+ElenaNicoara@users.noreply.github.com>
2023-03-19 21:31:47 -0400

    Homework26

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,18 +1,98 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
+import org.openqa.selenium.safari.SafariOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.*;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.time.Duration;
+import java.util.HashMap;
 
 
 public class BaseTest {
+    public static WebDriver driver = null;
+    private static final ThreadLocal<WebDriver> THREAD_LOCAL = new ThreadLocal<>();
+    public static String url = null;
 
     @BeforeSuite
-    static void setupClass() {
-        WebDriverManager.chromedriver().setup();
+    public void setupClass() {
+//        WebDriverManager.chromedriver().setup();
     }
+    @BeforeMethod
+    @Parameters({"baseUrl"})
+    public void launchBrowser(@Optional String baseUrl) throws MalformedURLException {
+        THREAD_LOCAL.set(pickBrowser("browser"));
+        THREAD_LOCAL.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        THREAD_LOCAL.get().manage().deleteAllCookies();
+        url = baseUrl;
+        getDriver().get(url);
+        System.out.println("Browser setup byThread" + Thread.currentThread().getId() + "and Driver reference is: " + getDriver());
+    }
+    public WebDriver lambdaTest() throws MalformedURLException {
+        String username = "nelena35";
+        String authkey = "6D9yzrrfh3h2EPqM7PBxkivvaSa3f20suqAfbajUojTSreJV36";
+        String hub = "@hub.lambdatest.com/wd/hub";
 
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("platform", "Windows 10");
+        caps.setCapability("browserName", "Chrome");
+        caps.setCapability("version", "110.0");
+        caps.setCapability("resolution", "1024x768");
+        caps.setCapability("build", "TestNG With Java");
+        caps.setCapability("name", this.getClass().getName());
+        caps.setCapability("plugin", "git-testng");
+        return new RemoteWebDriver(new URL("https://" + username + ":" + authkey + hub), caps);
+    }
+    public WebDriver pickBrowser(String browser) throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        String gridURL = "http://192.168.0.115:4444";
+        switch (browser) {
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                FirefoxOptions optionsFirefox = new FirefoxOptions();
+                optionsFirefox.addArguments("-private");
+                return driver = new FirefoxDriver(optionsFirefox);
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                return driver = new EdgeDriver();
+            case "grid-firefox":
+                caps.setCapability("browserName", "firefox");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
+            case "grid-edge":
+                caps.setCapability("browserName", "edge");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
+            case "grid-chrome":
+                caps.setCapability("browserName", "chrome");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
+            case "cloud":
+                return lambdaTest();
+            default:
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions optionsChrome = new ChromeOptions();
+                optionsChrome.addArguments("--remote-allow-origins=*","--disable-notifications","--incognito", "--start-maximized");
+                return driver = new ChromeDriver(optionsChrome);
+        }
+    }
+    @AfterMethod
+    public void closeBrowser(){
+        THREAD_LOCAL.get().close();
+        THREAD_LOCAL.remove();
+    }
+    public static WebDriver getDriver(){
+        return THREAD_LOCAL.get();
+    }
 }
---
+++ src/test/java/Homework26.java
@@ -0,0 +1,66 @@
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.BasePage;
+import pages.HomePage;
+import pages.LoginPage;
+import org.openqa.selenium.By;
+
+public class Homework26 extends BaseTest {
+
+    @Test(enabled = true, priority = 0, description = "Login with valid email and password")
+
+    public void loginWithValidCredentialsTest(){
+        LoginPage loginPage = new LoginPage(getDriver());
+        HomePage homePage = new HomePage(getDriver());
+
+        loginPage.login();
+        Assert.assertTrue(homePage.getUserAvatar().isDisplayed());
+    }
+    @Test(enabled = true, priority = 1, description = "Validate can create a playlist")
+    public void createNewPlaylistTest(){
+        LoginPage loginPage = new LoginPage(getDriver());
+        HomePage homePage = new HomePage(getDriver());
+
+        loginPage.login();
+        homePage.createPlaylist();
+        Assert.assertTrue(homePage.newPlaylistIsDisplayed());
+    }
+    @Test(enabled = true, priority = 2, description = "Validate can add a song to playlist")
+    public void addSongToExistingPlaylistTest()  {
+        LoginPage loginPage = new LoginPage(getDriver());
+        HomePage homePage = new HomePage(getDriver());
+
+        loginPage.login();
+        homePage.searchASong("Lost In Space")
+                .clickViewAllButton()
+                .selectFirstSong()
+                .clickAddToButton()
+                .clickPlaylistNameFromAddToButton("Sleepy Songs");
+        Assert.assertTrue(homePage.notificationMessageIsDisplayed());
+//        homePage.deleteSongFromPlaylist();
+    }
+    @Test(enabled = true, priority = 3, description = "Validate can rename an existing playlist")
+    public void renamePlaylistTest(){
+        LoginPage loginPage = new LoginPage(getDriver());
+        HomePage homePage = new HomePage(getDriver());
+
+        loginPage.login();
+        homePage.contextClickExistingPlaylist()
+                .chooseEdit()
+                .renamePlaylist("Active Songs");
+        Assert.assertTrue(homePage.displayUpdatedPlaylist("Active Songs"));
+    }
+    @Test (enabled = true, priority = 4, description = "Validate can delete a playlist")
+    public void deletePlaylistTest(){
+        LoginPage loginPage = new LoginPage(getDriver());
+        HomePage homePage = new HomePage(getDriver());
+
+        loginPage.login();
+        homePage.clickUpdatedPlaylist()
+//                .deleteExistingPlaylist();
+                .deleteFullPlaylist();
+        Assert.assertTrue(homePage.getNotificationMessage());
+    }
+
+}
