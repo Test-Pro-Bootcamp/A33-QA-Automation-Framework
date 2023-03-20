a889fb878e675bca3394e4edba770398834ccc1b
Mohamed Taqi <126124586+MedTaqi@users.noreply.github.com>
2023-03-19 20:40:34 -0400

    Homework21#

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -18,59 +18,68 @@ import java.time.Duration;
 public class BaseTest {
     static WebDriver driver;
     static WebDriverWait wait;
-    static String url=null;
+    static String url = null;
     public Actions actions;
     public String playlistName = "Homework21";
+
     @BeforeSuite
     public static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
+
     @BeforeMethod
     @Parameters({"baseURL"})
-    public void launchBrowser (String baseURL){
+    public void launchBrowser(String baseURL) {
         ChromeOptions ops = new ChromeOptions();
         ops.addArguments("--remote-allow-origins=*");
-        driver= new ChromeDriver(ops);
-        actions=new Actions(driver);
-        wait=new WebDriverWait(driver, Duration.ofSeconds(10));
-        url=baseURL;
+        driver = new ChromeDriver(ops);
+        actions = new Actions(driver);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        url = baseURL;
         driver.get(url);
         driver.manage().window().maximize();
     }
-    public void provideEmail(String email){
-        WebElement emailField=wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[type='email']")));
+
+    public void provideEmail(String email) {
+        WebElement emailField = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[type='email']")));
         emailField.clear();
         emailField.sendKeys(email);
     }
-    public void providePassword(String password){
-        WebElement passwordField=wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[type='password']")));
+
+    public void providePassword(String password) {
+        WebElement passwordField = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[type='password']")));
         passwordField.clear();
         passwordField.sendKeys(password);
     }
-    public void clickSubmit(){
-        WebElement submitButton=wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[type='submit']")));
+
+    public void clickSubmit() {
+        WebElement submitButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[type='submit']")));
         submitButton.click();
     }
-    public void logIn(String email,String password){
+
+    public void logIn(String email, String password) {
         provideEmail(email);
         providePassword(password);
         clickSubmit();
     }
-    public void doubleClick(){
+
+    public void doubleClick() {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#playlists > ul > li:nth-child(4)")));
-        WebElement playlistElement=driver.findElement(By.cssSelector("#playlists > ul > li:nth-child(4)"));
+        WebElement playlistElement = driver.findElement(By.cssSelector("#playlists > ul > li:nth-child(4)"));
         actions.doubleClick(playlistElement).perform();
     }
-    public void enterPlaylistName(){
-        WebElement inputField1=wait.until(ExpectedConditions.elementToBeClickable
+
+    public void enterPlaylistName() {
+        WebElement inputField1 = wait.until(ExpectedConditions.elementToBeClickable
                 (By.cssSelector("input[name='name']")));
-        inputField1.sendKeys(Keys.chord(Keys.COMMAND,"A"));
+        inputField1.sendKeys(Keys.chord(Keys.CONTROL, "A", Keys.BACK_SPACE));
         inputField1.sendKeys(playlistName);
         inputField1.sendKeys(Keys.ENTER);
     }
-    public boolean doesPlaylistExist(){
-        WebElement playlistElement=wait.until(ExpectedConditions.visibilityOfElementLocated
-                (By.xpath("//a[text()='"+playlistName+"']")));
+
+    public boolean doesPlaylistExist() {
+        WebElement playlistElement = wait.until(ExpectedConditions.visibilityOfElementLocated
+                (By.xpath("//a[text()='" + playlistName + "']")));
         return playlistElement.isDisplayed();
     }
 
--- src/test/java/Homework21.java
+++ src/test/java/Homework21.java
@@ -1,10 +1,10 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class Homework21 extends BaseTest{
+public class Homework21 extends BaseTest {
     @Test
     public void renamePlaylist() {
-        logIn("taqimed99@gmail.com","Med-20115-010499@");
+        logIn("taqimed99@gmail.com", "Med-20115-010499@");
         doubleClick();
         enterPlaylistName();
         Assert.assertTrue(doesPlaylistExist());
