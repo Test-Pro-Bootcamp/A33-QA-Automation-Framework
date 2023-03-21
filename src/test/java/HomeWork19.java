ed62bd12885131a4916b80128ea4760686764440
KristaOst <126209095+KristaOst@users.noreply.github.com>
2023-03-21 18:39:30 -0400

    Homework19

---
+++ src/test/java/HomeWork19.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HomeWork19  extends BaseTest {
+
+    @Test
+    public void deletePlaylist()  throws InterruptedException {
+        String deletePlaylistMsg ="Deleted playlist";
+        logIn();
+        openPlaylist();
+        clickDeletePlaylistBtn();
+        Assert.assertTrue(getDeletedPlaylistMsg().contains(deletePlaylistMsg));
+    }
+}
+
+
