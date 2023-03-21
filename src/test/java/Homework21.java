57e6fab391c6116c497175b9e06d5312338a9f17
DmitriiGreen <125938716+DmitriiGreen@users.noreply.github.com>
2023-03-21 14:18:56 -0700

    Update Homework21.java

--- src/test/java/Homework21.java
+++ src/test/java/Homework21.java
@@ -3,9 +3,9 @@ import org.testng.annotations.Test;
 
 public class Homework21 extends BaseTest {
 
-    @Test
-    public void test() {
-        renamePlaylist();
-        Assert.assertTrue(playlistDoesExist());
-    }
+//    @Test
+//    public void test() {
+//        renamePlaylist();
+//        Assert.assertTrue(playlistDoesExist());
+//    }
 }
