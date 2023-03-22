717784a4c1a20620453f817a08233c08efddd4c3
janezelenov <118363437+janezelenov@users.noreply.github.com>
2023-03-21 22:08:10 -0400

    ...

--- src/test/java/pages/stepDefinitions/LoginStepDefinitions.java
+++ src/test/java/pages/stepDefinitions/LoginStepDefinitions.java
@@ -1,13 +1,17 @@
 package pages.stepDefinitions;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 
 import java.time.Duration;
 
