be8dd3e851830d9e09fe1b178cc61f48ecf7a0d4
“Katerina <katerina.levchenko@testpro.io>
2023-03-24 16:13:28 +1000

    changed HW17, now it all works properly

--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -1,30 +1,27 @@
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import pages.BasePage;
+import pages.HomePage;
+import pages.LoginPage;
 
 public class Homework17 extends BaseTest {
 
 
     @Test
-    public void addSongToPlaylist () {
+    @Parameters ({"SongToSearch", "PlaylistToAddSong"})
+    public void addSongToPlaylist (String song, String playlist) {
 
         //variables
-        String playlist = "Motivating songs";
         String msg = "Added 1 song into";
-        String song = "Pluto";
-        String homeUrl = "https://bbb.testpro.io/";
 
-        //Navigate to "https://bbb.testpro.io/"
-        navigateToPage(homeUrl);
-        //Log in with your credentials
-        loginWithValidCredentials("me@elevchenko.com", "$student1111");
+        LoginPage loginPage = new LoginPage(getDriver());
+        HomePage homePage = new HomePage(getDriver());
+        BasePage basePage = new BasePage(getDriver());
+        basePage.navigateToPage(homeUrl);
+        loginPage.loginWithValidCredentials(email, password);
         //Search for a song (choose any song of your choice)
-        searchSong(song);
-        //Click View All to display the search results
-        viewAllSongs();
-        selectFirstSong(); //Click the first song
-
-        //Click ADD TO...  playlist, Verify that the notification message appears and says "Added 1 song into {your playlist}"
-        String msgReturn = clickAddToPls(playlist);
+        String msgReturn = homePage.searchSong(song).viewSearchedSong().clickAddToPls(playlist);
         Assert.assertTrue(msgReturn.contains(msg));
     }
 }
