0649c3426b7996e09bb108fb7f459dbcb43857e5
BravesFanatic <125943768+BravesFanatic@users.noreply.github.com>
2023-03-24 09:24:20 -0400

    Update to Homework17
    removed the spaces pointed out in feedback

--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -6,14 +6,8 @@ import org.testng.annotations.Test;
 public class Homework17 extends BaseTest {
     //extends BaseTests - inherits the methods /attributes of basetest class
 
-
     @Test
     public void addSongToPlaylist() throws InterruptedException {
-
-
-
-
-
         navigateToPage();
         login("demo@class.com", "te$t$tudent");
         searchSong("Gray");
@@ -24,16 +18,6 @@ public class Homework17 extends BaseTest {
 
         String expectedPopUpMsg = "Added 1 song into";
         Assert.assertTrue(getPlaylistAddedMsg().contains(expectedPopUpMsg));
-
-
-
-
-
-
-
-
-
-
 
         }
 //this is the closing curly bracket for the @Test case
@@ -72,6 +56,5 @@ public class Homework17 extends BaseTest {
         return actualPopUpMessage.getText();
 
     }
-
 
 }
