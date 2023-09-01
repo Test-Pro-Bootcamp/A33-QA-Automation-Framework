e30dde8c0505b32f072f751ac5fc63f92dfc6dc9
Mooka12 <140290284+Mooka12@users.noreply.github.com>
2023-09-01 13:16:56 -0500

    Merge pull request #229 from Test-Pro-Bootcamp/cucumber
    cucumber

--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -3,7 +3,7 @@ import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class Homework17 extends LoginTests{
+public class Homework17 extends BaseTest{
 
 @Test
 public void addSongToPlaylist() throws InterruptedException {
