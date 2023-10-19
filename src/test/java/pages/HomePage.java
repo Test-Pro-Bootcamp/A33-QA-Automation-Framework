e8864d28852bcf28129739b3574469cbb226cdb0
Mooka12 <140290284+Mooka12@users.noreply.github.com>
2023-10-19 15:43:03 -0500

    Merge pull request #229 from Test-Pro-Bootcamp/cucumber
    cucumber

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,13 +1,37 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 
-public class HomePage {
+By.firstPlaylist = By.cssSelector(".playlist:nth-child(3)");
+
+By.playlistInputField = By.cssSelector("input[name='name']");
+
+public class HomePage(WebDriver givenDriver) {
+    super(givenDriven);
+}
+
+
+public void doubleClickFirstPlaylist(){
+    doubleClickFirstPlaylist ();
+}
+
+public void enterPlaylistName(String playlistName) {
+    WebElement playlistNameTextField = findElement(playlistInputField);
+    playlistNameTextField.sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.BACK_SPACE)));
+    playlistNameTextField.sendKeys(playlistName);
+    playlistNameTextField.sendKeys(Keys.ENTER);
+}
+
+public boolean doesPlaylistExist(String playlistName) {
+    WebElement playlistElement = driver.findElement(By.xpath("//a[text()='" + playlistName + "']"));
+    return playlistElement.isDisplayed();
+}
 
     @FindBy(className = "avatar")
     public WebElement avatar;
